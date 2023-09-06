f47710cb95dd2e6ba00c244c283dacbc0c0d6de4
Walter Duque de Estrada <wbduque@mac.com>
2023-09-06 16:53:25 -0500

    Addressed comments and added 5 second delay

--- src/main/java/com/objectcomputing/greatdivider/GreatDividerGui.java
+++ src/main/java/com/objectcomputing/greatdivider/GreatDividerGui.java
@@ -1,6 +1,7 @@
 package com.objectcomputing.greatdivider;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.math.BigDecimal;
@@ -11,19 +12,19 @@ import java.util.regex.Pattern;
 public class GreatDividerGui {
 
     public static void main(String[] args) {
-        JFrame f=new JFrame();//creating instance of JFrame
+        JFrame f = new JFrame();//creating instance of JFrame
 
-        JTextField t1,t2;
-        t1=new JTextField("");
-        t1.setBounds(50,100, 100,30);
+        JTextField t1, t2;
+        t1 = new JTextField("");
+        t1.setBounds(50, 100, 100, 30);
         t1.setInputVerifier(new InputVerifier() {
             @Override
             public boolean verify(JComponent input) {
                 return (isNumeric(t1.getText()));
             }
         });
-        t2=new JTextField("");
-        t2.setBounds(50,150, 100,30);
+        t2 = new JTextField("");
+        t2.setBounds(50, 150, 100, 30);
         t2.setInputVerifier(new InputVerifier() {
             @Override
             public boolean verify(JComponent input) {
@@ -35,26 +36,40 @@ public class GreatDividerGui {
         f.add(t1);
         f.add(t2);
 
-        JButton b=new JButton("calculate");//creating instance of JButton
-        b.setBounds(50,200,100, 40);//x axis, y axis, width, height
-        final JLabel answer=new JLabel();
-        answer.setBounds(50,250, 100,30);
+        JButton b = new JButton("calculate");
+        f.getRootPane().setDefaultButton(b);
+        b.setBounds(50, 200, 100, 40);
+        final JLabel answer = new JLabel();
+        answer.setBounds(50, 250, 200, 30);
         answer.setText("Not Clicked");
+
         f.add(answer);
         b.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                DecimalFormat df = new DecimalFormat("#.######E00");
-                BigDecimal divide = new BigDecimal(t1.getText()).divide(new BigDecimal(t2.getText()), 7, RoundingMode.HALF_UP);
-                answer.setText(df.format(divide));
+                EventQueue.invokeLater(() -> {
+                    try {
+                        Thread.sleep(5 * 1000);
+                    } catch (InterruptedException ex) {
+                        // don't care
+                    }
+                    DecimalFormat df = new DecimalFormat("#.######E00");
+                    BigDecimal numerator = new BigDecimal(t1.getText());
+                    BigDecimal denominator = new BigDecimal(t2.getText());
+                    if (!denominator.equals(new BigDecimal(0))) {
+                        BigDecimal divide = numerator.divide(denominator, 7, RoundingMode.HALF_UP);
+                        answer.setText(df.format(divide));
+                    } else {
+                        answer.setText("Cant divide by zero");
+                    }
+                });
             }
         });
 
         f.add(b);//adding button in JFrame
 
 
-
-        f.setSize(400,500);//400 width and 500 height
+        f.setSize(400, 500);//400 width and 500 height
         f.setLayout(null);//using no layout managers
         f.setVisible(true);//making the frame visible
     }
@@ -67,7 +82,6 @@ public class GreatDividerGui {
         }
         return IS_NUMERIC.matcher(strNum).matches();
     }
-
 
 
 }
