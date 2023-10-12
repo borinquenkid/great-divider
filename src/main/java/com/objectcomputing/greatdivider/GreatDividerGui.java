af74a7d08e284837903d6b105e653e112ad0101c
Walter Duque de Estrada <wbduque@mac.com>
2023-10-12 09:38:27 -0500

    add calculator buttons and refactor

--- src/main/java/com/objectcomputing/greatdivider/GreatDividerGui.java
+++ src/main/java/com/objectcomputing/greatdivider/GreatDividerGui.java
@@ -18,7 +18,7 @@ public class GreatDividerGui {
     private static void createAndRun() {
         JFrame f = new JFrame();
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setSize(300, 300);
+        f.setSize(400, 400);
         JPanel glassPanel = new JPanel() {
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
@@ -28,51 +28,54 @@ public class GreatDividerGui {
         };
         glassPanel.setOpaque(false);
         f.setGlassPane(glassPanel);
-        JPanel p = createTabPanel(new MigLayout("inset 20"));
-        f.add(p);
+        JPanel primitivePanel = createTabPanel(new MigLayout("inset 20"));
+        f.add(primitivePanel);
         JTextField dividendField = createTextField("",3);
-        JTextField  divisorField = createTextField("",3);
-        JTextField  answerField = createTextField("",10);
+        JTextField divisorField = createTextField("",3);
+        JTextField answerField = createTextField("",16);
         answerField.setEnabled(false);
 
-        p.add(createLabel("Dividend",SwingConstants.LEADING), "gap para");
-        p.add(dividendField, "span");
-        p.add(createLabel("Divisor",SwingConstants.LEADING), "gap para");
-        p.add(divisorField, "span");
-        p.add(createLabel("Answer",SwingConstants.LEADING), "gap para");
-        p.add(answerField,"span");
-        p.add(createLabel("Status",SwingConstants.LEADING), "gap para");
+        primitivePanel.add(createLabel("Dividend",SwingConstants.LEADING), "gap para");
+        primitivePanel.add(dividendField, "span");
+        primitivePanel.add(createLabel("Divisor",SwingConstants.LEADING), "gap para");
+        primitivePanel.add(divisorField, "span");
+        primitivePanel.add(createLabel("Answer",SwingConstants.LEADING), "gap para");
+        primitivePanel.add(answerField,"span");
+        primitivePanel.add(createLabel("Status",SwingConstants.LEADING), "gap para");
         JLabel status = createLabel("",SwingConstants.LEADING);
-        p.add(status, "span");
+        primitivePanel.add(status, "span");
 
 
-        JButton calculateButton = new JButton("calculate");
-        p.getRootPane().setDefaultButton(calculateButton);
+        JButton calculateButton = new JButton("=");
+        primitivePanel.getRootPane().setDefaultButton(calculateButton);
 
         JProgressBar progressBar = new JProgressBar(0, 100);
         progressBar.setValue(0);
         progressBar.setStringPainted(true);
 
-        calculateButton.addActionListener(e -> {
-            DivisionWarningEnum result = new DivisionWarningGenerator().apply(dividendField.getText(), divisorField.getText());
-            if (result.equals(OK)) {
-                f.getGlassPane().setVisible(true);
-                answerField.setText("        ");
-                progressBar.setValue(0);
-                f.setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
-                dividendField.setEnabled(false);
-                divisorField.setEnabled(false);
-                calculateButton.setEnabled(false);
-                calculateButton.setText("Calculating");
-                new DividerWorker(dividendField, divisorField, answerField, calculateButton, progressBar,status, f).execute();
-            } else {
-                status.setText(result.getMessage());
-            }
-        });
+        calculateButton.addActionListener(new DivisionActionListener(dividendField,
+                divisorField,f,answerField,progressBar,calculateButton,status));
 
-        p.add(calculateButton, "span");//adding button in JFrame
-        p.add(progressBar, "span");
+        primitivePanel.add(calculateButton, "span");//adding button in JFrame
+        primitivePanel.add(progressBar, "span");
         f.setVisible(true);//making the frame visible
+        primitivePanel.add(new JButton("+"));
+        primitivePanel.add(new JButton("1"));
+        primitivePanel.add(new JButton("2"));
+        primitivePanel.add(new JButton("3"),"wrap");
+        primitivePanel.add(new JButton("-"));
+        primitivePanel.add(new JButton("4"));
+        primitivePanel.add(new JButton("5"));
+        primitivePanel.add(new JButton("6"),"wrap");
+        primitivePanel.add(new JButton("*"));
+        primitivePanel.add(new JButton("7"));
+        primitivePanel.add(new JButton("8"));
+        primitivePanel.add(new JButton("9"),"wrap");
+        primitivePanel.add(new JButton("/"));
+        primitivePanel.add(new JButton("."));
+        primitivePanel.add(new JButton("0"));
+        primitivePanel.add(new JButton("C"),"wrap");
+
     }
     protected static final Color GP_COLOR = new Color(0, 0, 0, 30);
 
